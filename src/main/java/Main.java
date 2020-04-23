b53d51c92fa2becf4d10b1d4a478bf37bf63bb74
Wismut <wismutmax@gmail.com>
2020-04-23 20:25:38 +0300

    Implements regions repository methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
+import model.Region;
 import repository.JavaIORegionRepository;
 
 public class Main {
     public static void main(String[] args) {
         JavaIORegionRepository regionRepository = new JavaIORegionRepository();
         System.out.println(regionRepository.getById(1L));
+        System.out.println(regionRepository.save(new Region(4L, "AL")));
+        System.out.println(regionRepository.getAll());
+        // TODO: 4/23/2020 Output is  [Region{id=1, name='UA'}, Region{id=2, name='YTR'}, Region{id=3, name='UK'}]
+        // but must be 4 regions. Regions with id equals 4 is absent in the output but present in the file regions.txt
     }
 }
--- src/main/java/repository/JavaIORegionRepository.java
+++ src/main/java/repository/JavaIORegionRepository.java
@@ -2,47 +2,89 @@ package repository;
 
 import model.Region;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JavaIORegionRepository {
-    private final String REGION_REPOSITORY_PATH = "files/regions.txt";
+    private final String REGION_REPOSITORY_PATH_WRITE = "src/main/resources/files/regions.txt";
+    private final String REGION_REPOSITORY_PATH_READ = "files/regions.txt";
+    private final String DELIMITER = ",";
 
     public Region getById(Long id) {
         if (id == null) {
             return null;
         }
-        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(REGION_REPOSITORY_PATH);
+        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(REGION_REPOSITORY_PATH_READ);
              BufferedReader reader = new BufferedReader(new InputStreamReader(resourceAsStream))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] split = line.split(",");
+                String[] split = line.split(DELIMITER);
                 if (id.equals(Long.parseLong(split[0]))) {
                     return new Region(Long.parseLong(split[0]), split[1]);
                 }
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
             return null;
         }
         return null;
     }
 
     public List<Region> getAll() {
-        return null;
+        List<Region> regions = new ArrayList<>();
+        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(REGION_REPOSITORY_PATH_READ);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(resourceAsStream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] split = line.split(DELIMITER);
+                regions.add(new Region(Long.parseLong(split[0]), split[1]));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return regions;
+        }
+        return regions;
     }
 
     public Region save(Region region) {
-        return null;
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(REGION_REPOSITORY_PATH_WRITE), StandardOpenOption.APPEND)) {
+            writer.append(String.valueOf(region.getId())).append(",").append(region.getName()).append('\n');
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return region;
     }
 
     public void deleteBy(Long id) {
-
+        if (id == null) {
+            return;
+        }
+        List<Region> filteredRegions = getAll().stream().filter(r -> !id.equals(r.getId())).collect(Collectors.toList());
+        System.out.println("filteredRegions: ");
+        System.out.println(filteredRegions);
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(REGION_REPOSITORY_PATH_WRITE))) {
+            for (Region region : filteredRegions) {
+                writer.append(String.valueOf(region.getId())).append(",").append(region.getName()).append('\n');
+                writer.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Region update(Region region) {
-        return null;
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(REGION_REPOSITORY_PATH_WRITE), StandardOpenOption.APPEND)) {
+            writer.append(String.valueOf(region.getId())).append(",").append(region.getName()).append('\n');
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return region;
     }
 }
