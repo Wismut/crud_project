fc4e88402a4f43aa37580c26a0a0144216fdbe4d
Sergiy <sfasfa@asd.sdgs>
2020-04-29 08:40:13 +0300

    Implements ViewFactory

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -2,5 +2,10 @@ package view;
 
 
 public interface View {
-
+    String USER_NUMBER = "1";
+    String POST_NUMBER = "2";
+    String REGION_NUMBER = "3";
+    String USER_LETTER = "u";
+    String POST_LETTER = "p";
+    String REGION_LETTER = "r";
 }
--- src/main/java/view/ViewFactory.java
+++ src/main/java/view/ViewFactory.java
@@ -2,13 +2,19 @@ package view;
 
 
 public class ViewFactory {
-
-
     public static View create(String type) {
         switch (type) {
-            case "1":
-//                return new UserView(new UserController(new JavaIOUserRepository()));
+            case View.USER_NUMBER:
+            case View.USER_LETTER:
+                return UserView.getInstance();
+            case View.POST_NUMBER:
+            case View.POST_LETTER:
+                return PostView.getInstance();
+            case View.REGION_NUMBER:
+            case View.REGION_LETTER:
+                return RegionView.getInstance();
+            default:
+                return null;
         }
-        return null;
     }
 }
