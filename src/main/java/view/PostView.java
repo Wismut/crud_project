1a6501436dd59f08f5a8ea9cb5113ea9253e48a3
Sergiy <sfasfa@asd.sdgs>
2020-04-29 14:31:14 +0300

    Implements postView execute

--- src/main/java/view/PostView.java
+++ src/main/java/view/PostView.java
@@ -6,11 +6,15 @@ import command.Command2;
 import controller.PostController;
 import model.Post;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class PostView extends View<Post, Long> {
     private final PostController postController;
     private static PostView instance;
+    public static final String LOCALDATETIME_PATTERN = "yyyy-MM-dd HH:mm";
 
     public static PostView getInstance() {
         if (instance == null) {
@@ -57,7 +61,30 @@ public class PostView extends View<Post, Long> {
     public void execute(Command2 command) {
         switch (command) {
             case DELETE_BY_ID:
-                postController.deleteById(1L);
+                String id = null;
+                try {
+                    System.out.println("Type post id");
+                    id = MainView.getReader().readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                postController.deleteById(Long.parseLong(id));
+                return;
+            case SAVE:
+                try {
+                    System.out.println("Type content");
+                    String content = MainView.getReader().readLine();
+                    System.out.println("Type created date and time in the format " + LOCALDATETIME_PATTERN);
+                    String created = MainView.getReader().readLine();
+                    System.out.println("Type updated date and time in the format " + LOCALDATETIME_PATTERN);
+                    String updated = MainView.getReader().readLine();
+                    Post post = new Post(content,
+                            LocalDateTime.parse(created, DateTimeFormatter.ofPattern(LOCALDATETIME_PATTERN)),
+                            LocalDateTime.parse(updated, DateTimeFormatter.ofPattern(LOCALDATETIME_PATTERN)));
+                    postController.save(post);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
         }
     }
 }
