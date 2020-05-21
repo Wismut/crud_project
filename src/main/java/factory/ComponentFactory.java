116402010b9d8cc845a20af0739ac7a3d2720f7c
Sergiy <sfasfa@asd.sdgs>
2020-05-21 18:57:08 +0300

    Update component factory

--- src/main/java/factory/ComponentFactory.java
+++ src/main/java/factory/ComponentFactory.java
@@ -1,30 +1,36 @@
 package factory;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ComponentFactory {
     private static final Map<Class, Object> componentByClass = new HashMap<>();
 
-//    clazz.getConstructor(PostRepository.class, RegionRepository.class)
-//    .newInstance(PostRepository.class.cast(componentByClass.get(PostRepository.class)),
-//    RegionRepository.class.cast(componentByClass.get(RegionRepository.class)))
-
     public static <T> T getBy(Class<T> clazz) {
         try {
             putConstructorParamsIfAbsent(clazz);
-            return clazz.cast(componentByClass.get(clazz));
-        } catch (InstantiationException | IllegalAccessException e) {
+            return clazz.cast(componentByClass.get(clazz.getInterfaces()[0]));
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static <T> void putConstructorParamsIfAbsent(Class<T> clazz) throws IllegalAccessException, InstantiationException {
-        if (clazz.getConstructors().length != 0) {
-            for (Class<?> parameterType : clazz.getConstructors()[0].getParameterTypes()) {
-                putConstructorParamsIfAbsent(parameterType);
+    private static <T> void putConstructorParamsIfAbsent(Class<T> clazz) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        if (clazz.getConstructors() != null && clazz.getConstructors().length != 0) {
+            if (clazz.getConstructors()[0].getParameterCount() == 0) {
+                componentByClass.putIfAbsent(clazz.getInterfaces()[0], clazz.newInstance());
+            } else {
+                for (Class<?> parameterType : clazz.getConstructors()[0].getParameterTypes()) {
+                    putConstructorParamsIfAbsent(parameterType);
+                }
+                Object[] objects = new Object[clazz.getConstructors()[0].getParameterCount()];
+                for (int i = 0; i < clazz.getConstructors()[0].getParameterCount(); i++) {
+                    objects[i] = componentByClass.get(clazz.getConstructors()[0].getParameterTypes()[i]);
+                }
+                componentByClass.putIfAbsent(clazz.getInterfaces()[0], clazz.getConstructor(clazz.getConstructors()[0].getParameterTypes())
+                        .newInstance(objects));
             }
-            componentByClass.putIfAbsent(clazz, clazz.newInstance());
         }
     }
 }
